df3b15ed754882408fcf03c98ed3779b83c4f91b
Philip Tempelman <tempelmanphilip@gmail.com>
2023-11-26 11:21:37 -0500

    add dataset & dataloader for vegetable

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from dataloader import dataset_vegetable, load_vegetable
 from model import resnet18
 
 
 if __name__ == "__main__":
     # TODO: initialize dataloader
+    train_dataset, val_dataset, test_dataset = dataset_vegetable()
+    train_dataloader, val_dataloader, test_dataloader = load_vegetable(
+        train_dataset, val_dataset, test_dataset, batch_size=32, num_workers=6
+    )
 
     model = resnet18(pretrained=False)
 
