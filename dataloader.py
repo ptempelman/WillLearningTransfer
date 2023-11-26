25030dfb9119f96a09a871a65cbcb30c52b377f4
Philip Tempelman <tempelmanphilip@gmail.com>
2023-11-26 14:00:53 -0500

    unify vegetable dataset and loading functionality

--- dataloader.py
+++ dataloader.py
@@ -1,7 +1,7 @@
 import torchvision
 from torchvision import transforms
 import os.path as osp
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 # TODO: create dataset
 
@@ -10,55 +10,47 @@ from torch.utils.data import DataLoader
 # TODO: later: apply data augmentation
 
 
-def dataset_vegetable():
-    train_dir = osp.join(".data", "vegetable", "train")
-    val_dir = osp.join(".data", "vegetable", "validation")
-    test_dir = osp.join(".data", "vegetable", "test")
+def create_dataset_vegetable():
+    train_dir = osp.join(".data", "vegetable")
 
-    train_transform = transforms.Compose(
-        [
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomRotation(15),
-            transforms.ColorJitter(
-                brightness=0.2, contrast=0.1, hue=0.1, saturation=0.1
-            ),
-            transforms.RandomAffine(
-                degrees=15, translate=(0.1, 0.1), scale=(1, 2), shear=15
-            ),
-            transforms.GaussianBlur(kernel_size=(5, 9)),
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-        ]
-    )
+    # train_transform = transforms.Compose(
+    #     [
+    #         transforms.RandomHorizontalFlip(p=0.5),
+    #         transforms.RandomRotation(15),
+    #         transforms.ColorJitter(
+    #             brightness=0.2, contrast=0.1, hue=0.1, saturation=0.1
+    #         ),
+    #         transforms.RandomAffine(
+    #             degrees=15, translate=(0.1, 0.1), scale=(1, 2), shear=15
+    #         ),
+    #         transforms.GaussianBlur(kernel_size=(5, 9)),
+    #         transforms.Resize((224, 224)),
+    #         transforms.ToTensor(),
+    #     ]
+    # )
 
-    test_transform = transforms.Compose(
+    transform = transforms.Compose(
         [
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
         ]
     )
 
-    train_dataset = torchvision.datasets.ImageFolder(
-        root=train_dir, transform=train_transform
-    )
-    val_dataset = torchvision.datasets.ImageFolder(
-        root=val_dir, transform=test_transform
-    )
-    test_dataset = torchvision.datasets.ImageFolder(
-        root=test_dir, transform=test_transform
-    )
+    dataset = torchvision.datasets.ImageFolder(root=train_dir, transform=transform)
 
-    return train_dataset, val_dataset, test_dataset
+    return dataset
 
 
-def load_vegetable(train_dataset, val_dataset, test_dataset, batch_size, num_workers=2):
-    train_dataloader = DataLoader(
-        train_dataset, batch_size, shuffle=True, num_workers=num_workers
+def load_vegetable(dataset, batch_size, num_workers=2):
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers
     )
-    val_dataloader = DataLoader(
-        val_dataset, batch_size, shuffle=True, num_workers=num_workers
+    val_loader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers
     )
-    test_dataloader = DataLoader(
-        test_dataset, batch_size, shuffle=False, num_workers=num_workers
-    )
-    return train_dataloader, val_dataloader, test_dataloader
+
+    return train_loader, val_loader
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from dataloader import dataset_vegetable, load_vegetable
+from dataloader import create_dataset_vegetable, load_vegetable
 from model import resnet18
 
 
 if __name__ == "__main__":
     # TODO: initialize dataloader
-    train_dataset, val_dataset, test_dataset = dataset_vegetable()
+    dataset_vegetable = create_dataset_vegetable()
     train_dataloader, val_dataloader, test_dataloader = load_vegetable(
-        train_dataset, val_dataset, test_dataset, batch_size=32, num_workers=6
+        dataset_vegetable, batch_size=32, num_workers=6
     )
 
     model = resnet18(pretrained=False)
